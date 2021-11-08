fb4dbbc244b6ec8e270f32924a6390af52ca453a
Robert <bates.to.infinity@gmail.com>
2021-11-08 16:12:37 -0600

    commit tree b21d268e16b709dd312078da32b3f0363f3c7211

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, name) {
+
+    const result = array.filter(names => names.toLowerCase() === name.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(array, name) {
+
+    const result = array.filter(names => names.charAt(0) === name.charAt(0))
+    return result
+}
+
+function matchName(array, name) {
+
+    const result = array.filter(names => names.name === name)
+    return result
+}
